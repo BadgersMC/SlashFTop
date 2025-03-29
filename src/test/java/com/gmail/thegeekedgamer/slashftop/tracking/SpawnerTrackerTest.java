34558641977bd01b7323cf65f7d7aa07f0d16d8d
BadgersMC <blrusso18@gmail.com>
2025-03-29 15:10:20 -0400

    test stuff

--- src/test/java/com/gmail/thegeekedgamer/slashftop/tracking/SpawnerTrackerTest.java
+++ src/test/java/com/gmail/thegeekedgamer/slashftop/tracking/SpawnerTrackerTest.java
@@ -1,8 +1,8 @@
-package com.thegeekedgamer.slashftop.tracking;
+package com.gmail.thegeekedgamer.slashftop.tracking;
 
 import com.gmail.thegeekedgamer.slashftop.*;
 import com.gmail.thegeekedgamer.slashftop.data.*;
-import com.gmail.thegeekedgamer.slashftop.tracking.*;
+import dev.rosewood.rosestacker.api.*;
 import net.milkbowl.vault.economy.*;
 import org.bukkit.*;
 import org.bukkit.block.*;
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.*;
 import org.mockito.*;
 import org.mockito.junit.jupiter.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,32 +33,37 @@ public class SpawnerTrackerTest {
     @Mock
     private BlockPlaceEvent event;
 
+    @Mock
+    private RoseStackerAPI roseStackerAPI;
+
     private SpawnerTracker spawnerTracker;
 
     @BeforeEach
     public void setUp() {
+        // Mock RoseStackerAPI.getInstance() to return our mock instance
+        mockStatic(RoseStackerAPI.class);
+        when(RoseStackerAPI.getInstance()).thenReturn(roseStackerAPI);
+
         spawnerTracker = new SpawnerTracker(plugin, economy);
         when(block.getType()).thenReturn(Material.SPAWNER);
         when(block.getState()).thenReturn(creatureSpawner);
         when(creatureSpawner.getSpawnedType()).thenReturn(org.bukkit.entity.EntityType.PIG);
         when(block.getLocation()).thenReturn(new Location(null, 0, 0, 0));
         when(event.getBlock()).thenReturn(block);
+        // Mock RoseStackerAPI.getStackedSpawner(block) to return null (no stacked spawner)
+        when(roseStackerAPI.getStackedSpawner(block)).thenReturn(null);
     }
 
     @Test
     public void testOnBlockPlace_TracksSingleSpawner() {
-        // Arrange
-        // No stacked spawner, so RoseStackerAPI.getInstance().getStackedSpawner(block) returns null
-        // (This is handled by MockitoExtension, which returns null by default for unstubbed methods)
-
         // Act
         spawnerTracker.onBlockPlace(event);
 
         // Assert
         Location location = block.getLocation();
         SpawnerData spawnerData = spawnerTracker.getTrackedSpawners().get(location);
-        assert spawnerData != null : "Spawner should be tracked";
-        assert spawnerData.getStackSize() == 1 : "Stack size should be 1 for a single spawner";
-        assert spawnerData.getSpawnerType().equals("PIG") : "Spawner type should be PIG";
+        assertNotNull(spawnerData, "Spawner should be tracked");
+        assertEquals(1, spawnerData.getStackSize(), "Stack size should be 1 for a single spawner");
+        assertEquals("PIG", spawnerData.getSpawnerType(), "Spawner type should be PIG");
     }
 }
